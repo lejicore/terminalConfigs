634adde69d8bea43ecd19ef58799afca4d01372e
S0mbr3 <benjamin_denis@outlook.com>
2026-04-02 11:25:18 +0200

    fix(neovim): prevent lua_ls to watch in useless dirs
    it was loading huge files that it cannot handles correctly without
    increasing it's buffer

--- .dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ .dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -199,6 +199,11 @@ return {
             workspace = {
               -- Make the server aware of Neovim runtime files
               library = vim.api.nvim_get_runtime_file("", true),
+              ignoreDir = {
+                "result",
+                ".direnv",
+                "/nix/store",
+              },
               checkThirdPAry = false,
             },
             -- Do not send telemetry data containing a randomized but unique identifier
