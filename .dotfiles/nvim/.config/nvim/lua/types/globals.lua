7cfa54e24ea64e57f60c1181cf676c91b95bc9ab
S0mbr3 <benjamin_denis@outlook.com>
2026-04-02 11:26:19 +0200

    fix(neovim): create global types
    lua_ls was complaining to not know some globals

--- .dotfiles/nvim/.config/nvim/lua/types/globals.lua
+++ .dotfiles/nvim/.config/nvim/lua/types/globals.lua
@@ -1,11 +1,28 @@
 ---@meta
 
----@class NvGlobals: vim.var_accessor
+---@class NvGlobals
 ---@field mapleader string
 ---@field maplocalleader string
 ---@field [string] any
 
+---@class NvOptValue
+local NvOptValue = {}
+
+---@param value string
+function NvOptValue:prepend(value) end
+
+---@param value string
+function NvOptValue:append(value) end
+
+---@class NvOpt
+---@field rtp NvOptValue
+---@field runtimepath NvOptValue
+---@field [string] any
+
 ---@type NvGlobals
 vim.g = vim.g
+
+---@type NvOpt
+vim.opt = vim.opt
 
 return {}
