5b5295041ef210a736a047020d16f9fb8c06ed12
S0mbr3 <benjamin_denis@outlook.com>
2026-05-13 00:44:15 +0200

    feat(nix-darwin): shell script to manage coc-db docker commands

--- .dotfiles/nix/darwin/packages.nix
+++ .dotfiles/nix/darwin/packages.nix
@@ -11,4 +11,21 @@ common-packages
   tree
   # needed for Emacs to use --dired in ls commands
   coreutils
+  docker-compose
+  (writeShellScriptBin "coc-db-up" ''
+    set -euo pipefail
+    exec docker compose -f ~/terminalConfigs/.dotfiles/nix/dev/postgres/coc-war-bot.compose.yml up -d
+  '')
+  (writeShellScriptBin "coc-db-down" ''
+    set -euo pipefail
+    exec docker compose -f ~/terminalConfigs/.dotfiles/nix/dev/postgres/coc-war-bot.compose.yml down
+  '')
+  (writeShellScriptBin "coc-db-logs" ''
+    set -euo pipefail
+    exec docker compose -f ~/terminalConfigs/.dotfiles/nix/dev/postgres/coc-war-bot.compose.yml logs -f postgres
+  '')
+  (writeShellScriptBin "coc-db-reset" ''
+    set -euo pipefail
+    exec docker compose -f ~/terminalConfigs/.dotfiles/nix/dev/postgres/coc-war-bot.compose.yml down -v
+  '')
 ]
