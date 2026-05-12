80b3492fc5a408b4fa59cd6ce99ccce817aac9fa
S0mbr3 <benjamin_denis@outlook.com>
2026-05-13 00:46:48 +0200

    fix(nixos): various overlay to fix rebuild problems

--- .dotfiles/nix/overlays/default.nix
+++ .dotfiles/nix/overlays/default.nix
@@ -1,20 +1,25 @@
-/* {inputs, ...}: {
-# This one brings our custom packages from the 'pkgs' directory
-additions = final: _prev: import ../pkgs {pkgs = final;};
+/*
+  {inputs, ...}: {
+  # This one brings our custom packages from the 'pkgs' directory
+  additions = final: _prev: import ../pkgs {pkgs = final;};
 
-# https://nixos.wiki/wiki/Overlays
-modifications = final: prev: {
-emacsLeji = import ./emacs.nix {inherit prev;};
-};
-} */
-{inputs}:
+  # https://nixos.wiki/wiki/Overlays
+  modifications = final: prev: {
+  emacsLeji = import ./emacs.nix {inherit prev;};
+  };
+  }
+*/
+{ inputs }:
 
 let
   inherit (inputs) emacs-src;
-in [
-  (import ./emacs.nix {inherit emacs-src;})
-  (final: prev:
-    {
+in
+[
+  (import ./emacs.nix { inherit emacs-src; })
+  # Darwin fixes for aider-achat and direnv, test to remove them later
+  (
+    final: prev:
+    prev.lib.optionalAttrs prev.stdenv.isDarwin {
       "aider-chat" = prev."aider-chat".overrideAttrs (_old: {
         doCheck = false;
         checkPhase = "true";
@@ -25,19 +30,42 @@ in [
         # Disable that feature to avoid the current deno/rustc ICE on darwin.
         javascriptSupport = false;
       };
-    })
+      "shtab" = prev.python3Packages.shtab.overrideAttrs (_: {
+        doCheck = false;
+      });
+      chromaprint = prev.chromaprint.overrideAttrs (_: {
+        doCheck = false;
+      });
+      kvazaar = prev.kvazaar.overrideAttrs (_: {
+        doCheck = false;
+      });
+      libcdio-paranoia = prev.libcdio-paranoia.overrideAttrs (old: {
+        postPatch = (old.postPatch or "") + ''
+          substituteInPlace src/getopt.h \
+            --replace 'extern int getopt ();' '/* extern int getopt (); */'
+        '';
+      });
+      direnv = prev.direnv.overrideAttrs (_: {
+        doCheck = false;
+      });
+    }
+  )
 ]
 
-/* {inputs}: {
-  
-  modifications = final: prev: {
-    emacsLeji = import ./emacs.nix {inherit prev;};
-  };
-} */
-/* {inputs}:
+/*
+  {inputs}: {
 
-[
-  (final: prev: {
-    emacsLeji = import ./emacs.nix {inherit prev;};
-  })
-] */
+    modifications = final: prev: {
+      emacsLeji = import ./emacs.nix {inherit prev;};
+    };
+  }
+*/
+/*
+  {inputs}:
+
+  [
+    (final: prev: {
+      emacsLeji = import ./emacs.nix {inherit prev;};
+    })
+  ]
+*/
